055fb3d4d80f76768787dc443a14e63e546132e9
Rashaan-Richardson <rashaan.richardson@gmail.com>
2023-06-13 15:49:30 -0600

    preparedness work

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,20 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Input, Label } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
 
 const App = () => {
+  const [name, setName] = useState("")
+  const nameInput = (input) => {
+    setName(input.target.value)
+  }
   return (
     <div className="entire-content">
       <h1>Preparedness Assessment</h1>
       <div className="form">
         <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
+          <Label for="name">Rashaan Richardson</Label>
+          <Input onChange={nameInput} value={name}/>
         </div>
-        <Button>Click Me</Button>
+        {/* <Button>Click Me</Button> */}
         <Button>Reset</Button>
-        <ModalComponent />
+        <ModalComponent name={name} />
       </div>
     </div>
   )
